254cabf6ac464161e6e3ebedd63421b87c7baeca
Christian Nyffenegger <freakpants@gmail.com>
2017-01-24 22:03:18 +0100

    moved password to config file, added config file and node_modules to gitignore

--- get_library.js
+++ get_library.js
@@ -1,6 +1,9 @@
+// get password from config file
+var pw = require('./config.js').pw();
+
 var PlayMusic = require('playmusic');
 var pm = new PlayMusic();
-pm.init({email: "freakpants@gmail.com", password: "emcebbogvjjpcrlh"}, function(err) {
+pm.init({email: "freakpants@gmail.com", password: pw }, function(err) {
     if(err) console.error(err);
 	pm.getAllTracks(function(err, library) {
 		console.log(JSON.stringify(library.data.items));
--- get_stream_url_by_track_id.js
+++ get_stream_url_by_track_id.js
@@ -1,12 +1,13 @@
+// get password from config file
+var pw = require('./config.js').pw();
 
 // determine parameters
 var nid = process.argv[2];
 
-
 var PlayMusic = require('playmusic');
 var pm = new PlayMusic();
 
-pm.init({email: "freakpants@gmail.com", password: "emcebbogvjjpcrlh"}, function(err) {
+pm.init({email: "freakpants@gmail.com", password: pw }, function(err) {
     if(err) console.error(err);
 
 	pm.getStreamUrl(nid, function(err, streamUrl) {	
