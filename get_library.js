f17e018c52df5c90f0417fba69aafbf9abc4d9c6
freakpants <freakpants@gmail.com>
2017-01-30 09:28:42 +0100

    remove self uplaoded tracks

--- get_library.js
+++ get_library.js
@@ -6,6 +6,13 @@ var pm = new PlayMusic();
 pm.init({email: email, password: pw }, function(err) {
     if(err) console.error(err);
 	pm.getAllTracks(function(err, library) {
+		// remove self uploaded songs
+		for( var item in library.data.items){
+			data_item = library.data.items[item];
+			if( typeof data_item.storeId === "undefined" ){
+				library.data.items.splice( item, 1 );
+			}
+		}
 		console.log(JSON.stringify(library.data.items));
 	});
 })
